f90c4e07a126938f4a8c41f4d70ec6c6bb772cb1
skyera <zliu@debian7>
2015-05-25 08:09:28 -0700

    atoi

---
+++ atoi.cpp
@@ -0,0 +1,76 @@
+// 5/25/2015
+//
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include<cassert>
+#include <climits>
+
+using namespace std;
+
+int myatoi(const string &str)
+{
+    int i = 0;
+    int size = str.size();
+
+    // skip white spaces
+    while (i < size && isspace(str[i])) {
+        i++;
+    }
+    
+    // str does not contain digits
+    if (i == size) {
+        return 0;
+    }
+    
+    // sign char?
+    bool is_neg = false;
+    char ch = str[i];
+    if (ch == '-') {
+        is_neg = true;
+        i++;
+    } else if (ch == '+') {
+        i++;
+    }
+
+    if (i == size) return 0;
+    
+    long long num = 0;
+    while (i < size && isdigit(str[i])) {
+        ch = str[i];
+        num = num * 10 + (ch - '0');
+        i++;
+    }
+
+    if (is_neg) {
+        num = -num;
+    }
+
+    if (num >= INT_MAX) return INT_MAX;
+    if (num <= INT_MIN) return INT_MIN;
+    return num;
+
+}
+
+void test(const string &str, int value)
+{
+    cout << "test " << str << "\n";
+
+    int val = myatoi(str);
+    bool ok = (val == value);
+    if (!ok) {
+        cout << "val=" << val << "\n";
+    }
+    assert(ok);
+
+}
+
+int main()
+{
+    test("1", 1);
+    test("x", 0);
+    test("-", 0);
+    test("-2", -2);
+    test("923372036854775809", 2147483647);
+}
